f158c8bc3bc38e8e130d8b75608fbf6d1c9a1395
Krisztian Pillis <pillis.krisztian@gmail.com>
2018-11-29 09:53:15 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Big Data Analysis of the Global Terrorism Database, Classification by number of victims.',
+    author='Krisztián Pillis, Áron Héthársi',
+    license='MIT',
+)
